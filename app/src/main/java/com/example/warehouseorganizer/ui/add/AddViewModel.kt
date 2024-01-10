6911b0b534957e720d6d5b342a240d95e7617d00
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2024-01-11 03:54:24 +0700

    Update AddViewModel for managing AddUIState and create methods to save items

--- app/src/main/java/com/example/warehouseorganizer/ui/add/AddViewModel.kt
+++ app/src/main/java/com/example/warehouseorganizer/ui/add/AddViewModel.kt
@@ -1,6 +1,6 @@
 package com.example.warehouseorganizer.ui.add
 
-import android.net.Uri
+import android.graphics.Bitmap
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -12,20 +12,13 @@ class AddViewModel(private val itemRepository: ItemRepository) : ViewModel() {
     var addUIState by mutableStateOf(AddUIState())
         private set
 
-    // New property to hold the selected image URI
-    var selectedImageUri by mutableStateOf<Uri?>(null)
-        private set
-
     fun updateAddUIState(addEvent: AddEvent) {
         addUIState = AddUIState(addEvent = addEvent)
     }
 
-    // Function to set the selected image URI
-    fun setSelectedImageUri(uri: Uri?) {
-        selectedImageUri = uri
+    suspend fun saveItem(bitmap: Bitmap, name: String, rack: String, quantity: Int) {
+        val result = itemRepository.saveItemWithImage(bitmap, name, rack, quantity)
+        println(result)
     }
 
-    suspend fun addItem() {
-        itemRepository.saveItem(addUIState.addEvent.toItem())
-    }
 }
