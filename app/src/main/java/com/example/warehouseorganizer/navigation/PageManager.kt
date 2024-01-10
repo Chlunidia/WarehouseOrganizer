43b44f5ca39af1cf8b69020e3f0e3dd5e7a7e31e
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2024-01-10 09:50:12 +0700

    Create composable HomeScreen with navigation and ViewModel integration

--- app/src/main/java/com/example/warehouseorganizer/navigation/PageManager.kt
+++ app/src/main/java/com/example/warehouseorganizer/navigation/PageManager.kt
@@ -2,35 +2,58 @@ package com.example.warehouseorganizer.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.warehouseorganizer.ui.home.DestinasiHome
+import com.example.warehouseorganizer.ui.ViewModelProviderFactory
+import com.example.warehouseorganizer.ui.add.AddScreen
 import com.example.warehouseorganizer.ui.home.HomeScreen
-import com.example.warehouseorganizer.ui.login.DestinasiLogin
 import com.example.warehouseorganizer.ui.login.LoginScreen
 import com.example.warehouseorganizer.ui.login.LoginViewModel
+import com.example.warehouseorganizer.ui.signup.SignUpScreen
+import com.example.warehouseorganizer.ui.signup.SignUpViewModel
 
 @Composable
-fun PageManager(
-    navController: NavHostController,
-    loginViewModel: LoginViewModel
-) {
+fun PageManager(navController: NavController, isLoggedIn: Boolean) {
     NavHost(
-        navController = navController,
-        startDestination = DestinasiLogin.route,
-        modifier = Modifier,
+        navController = navController as NavHostController,
+        startDestination = if (isLoggedIn) "HomeScreen" else "LoginScreen"
     ) {
-        composable(DestinasiLogin.route) {
+        composable("SignUpScreen") {
+            SignUpScreen(
+                navController = navController,
+                signUpViewModel = SignUpViewModel(),
+                onSignUpSuccess = {
+                    navController.navigate("LoginScreen")
+                }
+            )
+        }
+        composable("LoginScreen") {
             LoginScreen(
                 navController = navController,
-                loginViewModel = loginViewModel
-            ) {
-                navController.navigate(DestinasiHome.route)
-            }
+                loginViewModel = LoginViewModel(),
+                onLoginSuccess = {
+                    navController.navigate("HomeScreen")
+                }
+            )
         }
-        composable(DestinasiHome.route) {
-            HomeScreen(navController = navController)
+        composable("AddScreen") {
+            AddScreen(
+                navigateBack = { navController.popBackStack() },
+                modifier = Modifier,
+                addViewModel = viewModel(factory = ViewModelProviderFactory.Factory)
+            )
+        }
+        composable("HomeScreen") {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate("AddScreen")
+                },
+                onDetailClick = { itemId -> },
+                viewModel = viewModel(factory = ViewModelProviderFactory.Factory)
+            )
         }
     }
 }
